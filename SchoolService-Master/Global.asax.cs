a8b5b933e2611f46160273ce5f059bd99ef26d08
Abhishek <abhishek.karn@outlook.com>
2018-04-22 17:44:35 +0530

    Added code for creating new database in entityframework

--- SchoolService-Master/Global.asax.cs
+++ SchoolService-Master/Global.asax.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using SchoolService_Master.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Http;
@@ -13,6 +15,11 @@ namespace SchoolService_Master
     {
         protected void Application_Start()
         {
+            System.Data.Entity.Database.SetInitializer(new DropCreateDatabaseIfModelChanges<SchoolServiceContext>());
+            using (var context = new SchoolServiceContext())
+            {
+                context.Database.Initialize(force: true);
+            }
             AreaRegistration.RegisterAllAreas();
             GlobalConfiguration.Configure(WebApiConfig.Register);
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
